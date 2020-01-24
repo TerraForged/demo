dfaf1d8f73ec29710196d27ed9c1bb44d42fd49d
dags- <dags@dags.me>
2020-01-24 16:11:23 +0000

    init

---
+++ config.js
@@ -0,0 +1,56 @@
+var CONFIG = {
+  "maps": {
+    "world": {
+      "defaultZoom": 2,
+      "imageFormat": "jpg",
+      "lastRendered": [
+        1579880807,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 9,
+      "name": "world",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "world_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "world",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "world"
+  ],
+  "tileSetGroups": {
+    "world_isometric_t1": {
+      "maxZoom": 9,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
